e46caa0a7ff58ca4ce6c58acfa5f86f4bf87b74c
David Vargas <david.alfonso.v.q@gmail.com>
2025-07-14 11:56:18 -0400

    CHG: cambio en Readme y comentarios

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO USUARIO (name, email, password, token, isactived) VALUES ('Ana', 'ana@mail.com', 'contrasena', random_uuid(), true);
-INSERT INTO TELEFONO (number, citycode, contrycode, id_usuario) VALUES ('8855224', '32', '56', SELECT ID FROM USUARIO WHERE email = 'ana@mail.com' LIMIT 1);
+INSERT INTO USUARIO (name, email, password, token, isactived) VALUES ('Ana', 'ana@gmail.com', 'Contrasena5', random_uuid(), true);
+INSERT INTO TELEFONO (number, citycode, contrycode, id_usuario) VALUES ('8855224', '32', '56', SELECT ID FROM USUARIO WHERE email = 'ana@gmail.com' LIMIT 1);
